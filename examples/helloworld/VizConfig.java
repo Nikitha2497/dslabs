2935fbc95c9c23ac9a42a866acd2f2c756b0b295
kenmcmil <kenmcmil@users.noreply.github.com>
2022-02-08 20:31:38 +0000

    Committing built handout

---
+++ examples/helloworld/VizConfig.java
@@ -0,0 +1,51 @@
+package helloworld;
+
+import dslabs.framework.Command;
+import dslabs.framework.testing.StateGenerator;
+import dslabs.framework.testing.StateGenerator.StateGeneratorBuilder;
+import dslabs.framework.testing.Workload;
+import dslabs.framework.testing.search.SearchState;
+import dslabs.framework.testing.LocalAddress;
+//import dslabs.framework.testing.visualization.VizConfig;
+import helloworld.HelloApplication.Hello;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import static helloworld.Test.builder;
+
+public class VizConfig extends dslabs.framework.testing.visualization.VizConfig {
+    @Override
+    public SearchState getInitialState(int numServers, int numClients,
+                                       List<String> commands) {
+        SearchState searchState =
+                super.getInitialState(0, numClients, commands);
+        searchState.addServer(Test.sa);
+        return searchState;
+    }
+
+    @Override
+    protected StateGenerator stateGenerator(List<String> workload) {
+        StateGeneratorBuilder builder = builder();
+        //        builder.workloadSupplier(__ -> Workload.workload(
+        //        workload.stream().map(Hello::new).collect(Collectors.toList())));
+        builder.workloadSupplier(a -> {
+                List<Command> cs = new ArrayList<Command>();
+                for (String s : workload) {
+                    List<String> fs = Arrays.asList(s.split(":"));
+                    if (fs.size() == 1) {
+                        cs.add(new Hello(fs.get(0)));
+                    } else if (fs.size() == 2) {
+                        if (a.equals(new LocalAddress(fs.get(0)))) {
+                            cs.add(new Hello(fs.get(1)));
+                        } 
+                    } else {
+                        throw new IllegalArgumentException();
+                    }
+                }
+                return Workload.workload(cs);
+            });
+        return builder.build();
+    }
+}
---
+++ labs/lab1-sharedobject/tst/dslabs/vizconfigs/Lab1VizConfig.java
@@ -0,0 +1,61 @@
+package dslabs.vizconfigs;
+
+import dslabs.framework.Command;
+import dslabs.framework.Address;
+import dslabs.framework.testing.StateGenerator;
+import dslabs.framework.testing.StateGenerator.StateGeneratorBuilder;
+import dslabs.framework.testing.Workload;
+import dslabs.framework.testing.search.SearchState;
+import dslabs.framework.testing.LocalAddress;
+//import dslabs.framework.testing.visualization.VizConfig;
+import dslabs.kvstore.KVStore.Append;
+import dslabs.kvstore.KVStore.Get;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import static dslabs.sharedobject.ChainRepAppendTest.builder;
+
+public class Lab1VizConfig extends dslabs.framework.testing.visualization.VizConfig {
+    @Override
+    public SearchState getInitialState(int numServers, int numClients,
+                                       List<String> commands) {
+        SearchState searchState =
+                super.getInitialState(0, numClients, commands);
+        for (Address sa : dslabs.sharedobject.ChainRepAppendTest.servers) {
+            searchState.addServer(sa);
+        }
+        return searchState;
+    }
+
+    @Override
+    protected StateGenerator stateGenerator(List<String> workload) {
+        StateGeneratorBuilder builder = builder();
+        builder.workloadSupplier(a -> {
+                List<Command> cs = new ArrayList<Command>();
+                for (String s : workload) {
+                    List<String> fs = Arrays.asList(s.split(":"));
+                    if (fs.size() == 1) {
+                        cs.add(new Append("k",fs.get(0)));
+                    } else if (fs.size() == 2 && fs.get(1).equals("Get")) {
+                        System.out.println("fs: " + fs + " a: " + a);
+                        if (a.equals(new LocalAddress(fs.get(0)))) {
+                            cs.add(new Get("k"));
+                        } 
+                    } else if (fs.size() == 3 && fs.get(1).equals("Append")) {
+                        System.out.println("fs: " + fs + " a: " + a);
+                        if (a.equals(new LocalAddress(fs.get(0)))) {
+                            cs.add(new Append("k",fs.get(2)));
+                        } 
+                    } else {
+                        throw new IllegalArgumentException();
+                    }
+                }
+                return Workload.workload(cs);
+            });
+        //        builder.workloadSupplier(__ -> Workload.workload(
+        //        workload.stream().map(Append::new).collect(Collectors.toList())));
+        return builder.build();
+    }
+}
